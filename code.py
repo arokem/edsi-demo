4f473419f2e333082fe7417a08187c16b7acd16b
Ariel Rokem <arokem@gmail.com>
2019-10-23 15:45:18 -0700

    Added docstring to area function

--- code.py
+++ code.py
@@ -1,4 +1,15 @@
 import numpy as np
 
 def area(radius):
+    """Calculate the area of a circle.
+
+    Parameters
+    ----------
+    radius : float
+        The radius of a circle.
+
+    Returns
+    -------
+    area : float
+    """
     return np.pi * (radius ** 2)
